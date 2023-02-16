8161eb70e0723b32c2e2612540f9ab608ab1dd79
direktor2007 <>
2023-02-16 21:37:43 +0700

    work1

---
+++ Work/work1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Random rand = new Random ();
+int number =  new Random().Next (100, 1000);
+System.Console.WriteLine($"Случайное число:{number}");
+
+//System.Console.WriteLine($"{number / 100}{number % 10}");
+number = ((number % 100)) - (number % 10);
+
+System.Console.WriteLine($"{number}");
+
+
+
+
