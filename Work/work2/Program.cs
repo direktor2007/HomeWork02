0088adc20736b0815e312627de56e23fec714d83
direktor2007 <>
2023-02-17 23:22:17 +0700

    work

---
+++ Work/work2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+
+System.Console.WriteLine("Введите число:");
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+int N = numberA.ToString().Length;
+if (N < 3)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int A = N - 3;
+    int result = numberA / (Convert.ToInt32(Math.Pow(10, A))) % 10;
+    System.Console.WriteLine(result);
+}
